3c2817be588dbf74438a97640107a6b1da7c5b3f
Qboi <30625109+Qboi123@users.noreply.github.com>
2022-07-19 12:48:39 +0200

    Fixed mining of block.

--- src/main/java/com/ultreon/masterweapons/init/ModBlocks.java
+++ src/main/java/com/ultreon/masterweapons/init/ModBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,10 +16,10 @@ import java.util.function.Supplier;
 @SuppressWarnings("unused")
 public class ModBlocks {
     static final DeferredRegister<Block> REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, MasterWeapons.MOD_ID);
-    public static final RegistryObject<Block> ULTRAN_BLOCK = register("ultran_block", () -> new Block(Block.Properties.of(Material.STONE)));
-    public static final RegistryObject<Block> RAW_ULTRAN_BLOCK = register("raw_ultran_block", () -> new Block(Block.Properties.of(Material.STONE)));
-    public static final RegistryObject<Block> ULTRAN_ORE = register("ultran_ore", () -> new Block(Block.Properties.of(Material.STONE)));
-    public static final RegistryObject<Block> DEEPSLATE_ULTRAN_ORE = register("deepslate_ultran_ore", () -> new Block(Block.Properties.of(Material.STONE)));
+    public static final RegistryObject<Block> ULTRAN_BLOCK = register("ultran_block", () -> new Block(Block.Properties.of(Material.HEAVY_METAL, MaterialColor.COLOR_RED).requiresCorrectToolForDrops().strength(200f, 5000f)));
+    public static final RegistryObject<Block> RAW_ULTRAN_BLOCK = register("raw_ultran_block", () -> new Block(Block.Properties.of(Material.HEAVY_METAL, MaterialColor.COLOR_RED).requiresCorrectToolForDrops().strength(200f, 5000f)));
+    public static final RegistryObject<Block> ULTRAN_ORE = register("ultran_ore", () -> new Block(Block.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).requiresCorrectToolForDrops().strength(200f, 5000f)));
+    public static final RegistryObject<Block> DEEPSLATE_ULTRAN_ORE = register("deepslate_ultran_ore", () -> new Block(Block.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).requiresCorrectToolForDrops().strength(200f, 5000f)));
 
     private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> supplier) {
         RegistryObject<T> registryObject = REGISTER.register(name, supplier);
