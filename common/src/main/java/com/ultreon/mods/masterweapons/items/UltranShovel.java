0edbf64e10a2fb86b1e2c3dada3cbeb40eb90a7d
xypercode <qboiwastaken@gmail.com>
2024-04-28 08:37:00 +0200

    Fix crash when hitting mobs causing infinite motion.

--- common/src/main/java/com/ultreon/mods/masterweapons/items/UltranShovel.java
+++ common/src/main/java/com/ultreon/mods/masterweapons/items/UltranShovel.java
@@ -84,7 +84,7 @@ public class UltranShovel extends ShovelItem implements UltranToolBase, ItemExte
         if (equipmentSlot == EquipmentSlot.MAINHAND) {
             multimap.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", Double.POSITIVE_INFINITY, AttributeModifier.Operation.ADDITION));
             multimap.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", Double.POSITIVE_INFINITY, AttributeModifier.Operation.ADDITION));
-            multimap.put(Attributes.ATTACK_KNOCKBACK, new AttributeModifier(BASE_ATTACK_KNOCKBACK_UUID, "Weapon modifier", Double.POSITIVE_INFINITY, AttributeModifier.Operation.ADDITION));
+            multimap.put(Attributes.ATTACK_KNOCKBACK, new AttributeModifier(BASE_ATTACK_KNOCKBACK_UUID, "Weapon modifier", 2500.0, AttributeModifier.Operation.ADDITION));
         }
         return multimap;
     }
